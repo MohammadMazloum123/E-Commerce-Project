fdde58a5bf2006096f8fb1f10db31c7ace92b4db
ba3as <144138337+ba3as@users.noreply.github.com>
2024-06-03 19:29:50 +0300

    Project

---
+++ .history/images/app_20240218162550.js
@@ -0,0 +1,39 @@
+const cartBtn = document.querySelector(".cart-btn")
+const closeCartBtn = document.querySelector(".close-cart")
+const clearCartBtn = document.querySelector(".clear-cart")
+const cartDOM = document.querySelector(".cart")
+const cartOverlay = document.querySelector(".cart-overlay")
+const cartItems = document.querySelector(".cart-items")
+const cartTotal = document.querySelector(".cart-total")
+const cartContent = document.querySelector(".cart-content")
+const productsDOM = document.querySelector(".products-center")
+
+let cart=[]
+
+// getting the products
+class Products{
+    async getProducts(){
+    try {
+        let result = await fetch("products.json");
+        let data = await result.json();
+        let products = data.items 
+        products = products.map(item =>{
+            const {title, price} = 
+        })
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+}
+// display products
+class UI{}
+// local storage
+class Storage{}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const products = new Products();
+    const ui = new UI();
+    // get all products
+    products.getProducts().then(data => console.log(data));
+})
